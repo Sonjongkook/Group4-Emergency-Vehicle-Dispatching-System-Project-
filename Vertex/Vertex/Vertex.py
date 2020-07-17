9232dcaedaf59f647c11e0dc27a0cc6d201366d9
Kyle Son <sjk378@gmail.com>
2020-07-17 11:42:01 -0500

    Update Vertex.py
    v --> self (Method '__init__' should have 'self' as first argument.Python(no-self-argument))

--- Vertex/Vertex/Vertex.py
+++ Vertex/Vertex/Vertex.py
@@ -2,29 +2,29 @@ import sys
 
 class Vertex:
 	#initialize member variables of the vertex
-	def __init__(v, node):
-		v.value = node
-		v.neighbor = {}
-		v.distance = float('inf')
-		v.visited = False
+	def __init__(self, node):
+		self.value = node
+		self.neighbor = {}
+		self.distance = float('inf')
+		self.visited = False
 
-	def add_neighbor(v, adjacent, weight):
+	def add_neighbor(self, adjacent, weight):
 		self.neighbor[adjacent] = weight
 
-	def get_value(v):
-		return v.value
+	def get_value(self):
+		return self.value
 
-	def get_weight(v, adjacent):
-		return v.neighbor[adjacent]
+	def get_weight(self, adjacent):
+		return self.neighbor[adjacent]
 
-	def set_distance(v, d):
-		v.distance = d
+	def set_distance(self, d):
+		self.distance = d
 
-	def get_distance(v):
-		return v.distance
+	def get_distance(self):
+		return self.distance
 
-	def set_visited(v):
-		v.visited = True
+	def set_visited(self):
+		self.visited = True
 
 
 
