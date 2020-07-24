ab3d74c24c50c8b503237a212fc021e5205a7056
Sonjongkook <63791870+Sonjongkook@users.noreply.github.com>
2020-07-24 11:45:36 -0500

    update for main
    update for main

--- project/main.py
+++ project/main.py
@@ -1,11 +1,14 @@
 import json
+#from json2html import *
+#import sys
 from graph import Graph
 from vertex import Vertex
 from dijkstra import dijkstra
 
 if __name__ == '__main__':
     opened_data = json.load(open('data.json', 'r'))
-    #opened_data = json.load(open('data2.json', 'r'))
+    #opened_data = json.load(open('data2.json', 'r')) (another json data)
+    files = json2html.convert(json = opened_data)
     vehicles = opened_data['vehicles']  # getting vehicle
     requsets = opened_data['requests']
     distances = opened_data['distances']
@@ -34,16 +37,19 @@ if __name__ == '__main__':
         if avaliable_vehicles:
             graph.reset_verticles()  # we need to initialize the status to implement dijkstra
             # using a dijkstra method
+            # assign start point as requste node
             dijkstra(graph, graph.get_vertax(requset['zipcode']))
 
         for aval in avaliable_vehicles:
             aval['distance'] = graph.get_vertax(
                 aval['zipcode']).get_distance()  # get a distance from a dijkstra
+                
         # sort based on distance so we can find minimum one as an best one
         avaliable_vehicles = sorted(
             avaliable_vehicles, key=lambda x: x['distance'])
 
         if avaliable_vehicles:
+            print("Using Dijkstra algorith")
             for aval in avaliable_vehicles:
                 print("id {} vehicle is ready at zipcode {} and distance is {}".format(aval['id'],
                                                                                        aval['zipcode'], aval['distance']))
